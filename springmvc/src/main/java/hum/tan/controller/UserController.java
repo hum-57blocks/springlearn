303214fb98fd7823dc4b3099c5eeab9fbf19837d
hum <hum@57blocks.com>
2023-05-20 23:29:13 +0800

    springmvc项目配置

---
+++ springmvc/src/main/java/hum/tan/controller/UserController.java
@@ -0,0 +1,13 @@
+package hum.tan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+    @RequestMapping("/user")
+    public String save() {
+        System.out.println("UserController save running...");
+        return "success.jsp";
+    }
+}
