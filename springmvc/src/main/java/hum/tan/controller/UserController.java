028f2a1d02f61b38f508499c37875e53be1361dd
hum <hum@57blocks.com>
2023-06-03 13:09:55 +0800

    配置springmvc的视图解析器prefix和suffix

--- springmvc/src/main/java/hum/tan/controller/UserController.java
+++ springmvc/src/main/java/hum/tan/controller/UserController.java
@@ -2,12 +2,14 @@ package hum.tan.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
-    @RequestMapping("/user")
+    @RequestMapping(value = "/quick", method = RequestMethod.GET, params = {"username"})
     public String save() {
         System.out.println("UserController save running...");
-        return "success.jsp";
+        return "success";
     }
 }
