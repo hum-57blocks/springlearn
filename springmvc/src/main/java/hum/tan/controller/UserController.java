07af65bd01e7f0312c8bd2778776da2791dd7ef6
hum <hum@57blocks.com>
2023-06-04 19:10:34 +0800

    在springmvc中返回json字符串

--- springmvc/src/main/java/hum/tan/controller/UserController.java
+++ springmvc/src/main/java/hum/tan/controller/UserController.java
@@ -1,25 +1,60 @@
 package hum.tan.controller;
 
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import hum.tan.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    @RequestMapping(value = "/quick8")
+    @ResponseBody
+    public String save8() throws JsonProcessingException {
+        User user = new User();
+        user.setUsername("hum tan");
+        user.setAge(18);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(user);
+    }
+
+    @RequestMapping(value = "/quick7")
+    @ResponseBody
+    public String save7() {
+        return "{\"username\":\"hum tan\", \"age\": 18}";
+    }
+
+    @RequestMapping(value = "/quick6")
+    @ResponseBody
+    public String save6() {
+        return "hum tan";
+    }
+
+    @RequestMapping(value = "/quick5")
+    public void save5(HttpServletResponse response) throws IOException {
+        response.getWriter().print("hum tan");
+    }
+
     @RequestMapping(value = "/quick4")
-    public String save2(HttpServletRequest request) {
+    public String save4(HttpServletRequest request) {
         request.setAttribute("username", "hum4");
         return "success";
     }
 
     @RequestMapping(value = "/quick3")
-    public String save2(Model model) {
+    public String save3(Model model) {
         model.addAttribute("username", "hum3");
         return "success";
     }
