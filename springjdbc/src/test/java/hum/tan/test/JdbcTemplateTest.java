90e52a09c2bc77cbb2be0d1f02fb4f471311460a
hum <hum@57blocks.com>
2023-06-07 09:48:22 +0800

    使用spring生成JdbcTemplate对象

--- springjdbc/src/test/java/hum/tan/test/JdbcTemplateTest.java
+++ springjdbc/src/test/java/hum/tan/test/JdbcTemplateTest.java
@@ -2,9 +2,21 @@ package hum.tan.test;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class JdbcTemplateTest {
+    // 测试spring产生JdbcTemplate对象
+    @Test
+    public void test2() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
+        int row = jdbcTemplate.update("insert into account values (?, ?)", "jolin", 21.5);
+        System.out.println(row);
+    }
+
     // 测试JdbcTemplate开发步骤
     @Test
     public void test1() {
